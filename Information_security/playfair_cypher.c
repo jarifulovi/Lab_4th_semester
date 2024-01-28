297343a9fd4526acdffce36849aa15555dfcccec
ovi <jarifulovi@gmail.com>
2024-01-28 22:36:51 +0600

    1/28/2024

--- Information_security/playfair_cypher.c
+++ Information_security/playfair_cypher.c
@@ -1,7 +1,118 @@
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+char key[5][5];
+
+void printkey(char key[5][5]){
+     for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            printf("%c ",key[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void generateKey(char keyword[]) {
+  
+    char uniqueKey[26] = {0};
+    int uniqueKeyIndex = 0;
+
+    for (int i = 0; i < strlen(keyword); i++) {
+        // if the keyword charcter is an alphabet
+        if (isalpha(keyword[i])) {
+            // transform the character into lower case
+            char currentChar = tolower(keyword[i]);
+            // if the character is not j and not in the uniqueKey array
+            if (currentChar != 'j' && !strchr(uniqueKey, currentChar)) {
+                uniqueKey[uniqueKeyIndex++] = currentChar;
+            }
+        }
+    }
+
+    // fill the remaining positions in the uniqueKey array with the alphabet
+    for (char ch = 'a'; ch <= 'z'; ch++) {
+        if (ch != 'j' && !strchr(uniqueKey, ch)) {
+            uniqueKey[uniqueKeyIndex++] = ch;
+        }
+    }
+
+    // fill the keyTable using the uniqueKey array
+    int k = 0;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            key[i][j] = uniqueKey[k++];
+        }
+    }
+}
+// finding the position (row and col) of a character
+void findPosition(char ch,int* row,int* col){
+     for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if(key[i][j]==ch){
+                *row = i;
+                *col = j;
+            }
+        }
+    }
+}
+
+
+void generateCypher(char string[]){
+
+    int row1=0,row2=0,col1=0,col2=0;
+    int stringLength = strlen(string);
+    char bongusChar = 'z';
+    char cypher[100];
+    int cypherIndex = 0;
+
+    for(int index=0;index<strlen(string);index++){
+        char first = string[index++];
+        char second;
+        // if the string becomes odd then adding the bongus character
+        if(index<strlen(string)){
+            second = string[index];
+        }
+        else {
+            second = bongusChar;
+        }
+        // if the characters are same then add 'x'
+        if(first==second){
+            second = 'x';
+            index--;
+        }
+
+        findPosition(first,&row1,&col1);
+        findPosition(second,&row2,&col2);
+
+        if(row1==row2){
+            cypher[cypherIndex++] = key[row1][(col1+1)%5];
+            cypher[cypherIndex++] = key[row2][(col2+1)%5];
+        }
+        else if(col1==col2){
+            cypher[cypherIndex++] = key[(row1+1)%5][col1];
+            cypher[cypherIndex++] = key[(row2+1)%5][col2];
+        }
+        else {
+            cypher[cypherIndex++] = key[row1][col2];
+            cypher[cypherIndex++] = key[row2][col1];
+        }
+        
+    }
+    cypher[cypherIndex] = '\0';
+    printf("The cypher string is %s",cypher);
+}
 
 int main(){
-
-
+    char keyword[100];
+    char string[100];
+    printf("Enter the keyword\n");
+    scanf(" %s",&keyword);
+    printf("Enter the string \n");
+    scanf(" %s",&string);
+    // first create the 5x5 board of characters
+    generateKey(keyword);
+    printkey(key);
+    // generates the cypher of the string
+    generateCypher(string);
     return 0;
 }
---
+++ Information_security/viginere_cypher.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+char charTable[26][27];
+
+
+void printTable(){
+    for(int i=0;i<26;i++){
+        for(int j=0;j<26;j++){
+            printf("%c ",charTable[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void generateTable(){
+
+    int i,j;
+    for(i=0;i<26;i++){
+      
+        for(j=0;j<26;j++){
+            int character = 'a' + ((i+j) % 26);
+            charTable[i][j] = character;
+        }
+        charTable[i][j] = '\0';
+    }
+}
+void expandKeyword(char keyword[100],char newKeyword[100],int sl,int kl){
+
+    int i;
+    for (i = 0; i < sl; i++)
+    {
+        newKeyword[i] = keyword[i%kl];
+    }
+    newKeyword[i] = '\0';
+}
+
+
+void generateCypher(char keyword[100],char string[100],char cypher[100]){
+    
+    // keyword should not be larger than string
+    if(strlen(keyword)>strlen(string)){
+        printf("keyword is small\n");
+        return;
+    }
+    // expand the keyword into newkeyword
+    char newKeyword[100];
+    int stringLength = strlen(string);
+    int keywordLength = strlen(keyword);
+    expandKeyword(keyword,newKeyword,stringLength,keywordLength);
+    
+    for (int i = 0; i < strlen(string); i++)
+    {
+        int row = (tolower(string[i]) - 97);
+        int col = (tolower(newKeyword[i]) - 97);
+
+        cypher[i] = charTable[row][col];
+    }
+    
+}
+void decryption(char keyword[100],char string[100],char decryptString[100]){
+    // first expand the keyword
+   
+     // keyword should not be larger than string
+    if(strlen(keyword)>strlen(string)){
+        printf("keyword is small\n");
+        return;
+    }
+    // expand the keyword into newkeyword
+    char newKeyword[100];
+    int stringLength = strlen(string);
+    int keywordLength = strlen(keyword);
+
+    expandKeyword(keyword,newKeyword,stringLength,keywordLength);
+    int i;
+    for (i = 0; i < stringLength; i++)
+    {
+        int row = 0;
+        // Adjust for negative values
+        int col = (tolower(string[i]) - tolower(newKeyword[i]) + 26) % 26;
+
+        decryptString[i] = charTable[row][col];
+    }
+    decryptString[i] = '\0';
+}
+
+int main() {
+    char keyword[100],string[100],cypher[100],decryptString[100];
+    printf("Enter the keyword : ");
+    scanf(" %s",keyword);
+    printf("Enter the string : ");
+    scanf(" %s",string);
+    generateTable();
+    printTable();
+    generateCypher(keyword,string,cypher);
+    printf("The encrypted string : %s\n",cypher);
+    decryption(keyword,cypher,decryptString);
+    printf("The decrypted string : %s\n",decryptString);
+    return 0;
+}
