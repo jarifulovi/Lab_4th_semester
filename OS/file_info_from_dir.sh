891568d61d74d50fde7f428f4d91aa014d3070ea
ovi <ovi@UBUNTU.myguest.virtualbox.org>
2024-02-05 23:29:03 +0600

    modify_os

--- OS/file_info_from_dir.sh
+++ OS/file_info_from_dir.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "enter directory path : "
+
 read dirPath
 
+echo "enter a specific type : (empty for all file types)"
+
+read -r file_type
 
 if [ -d $dirPath ]; then
 
@@ -13,15 +17,17 @@ if [ -d $dirPath ]; then
 	totalLine=0;
 	for file in $files; do 
 		
-		if [ -f $file ]; then
+		if [ -f "$file" ] && { [ -z "$file_type" ] || [[ $file =~ \.($file_type)$ ]]; }; then
+        
 		
 		name=$(basename $file)
 		size=$(du -b $file | cut -f1)
+		size_in_readable=$(du -h $file | cut -f1)
 		info=$(stat $file)
 		fileLine=$(wc -l < $file)
 
 		echo "file name : $name"
-		echo "file size : $size"
+		echo "file size : $size_in_readable"
 		echo "lines in the file : $fileLine"
 		
 		((totalFiles++))
@@ -32,7 +38,12 @@ if [ -d $dirPath ]; then
 		fi
 
 	done
+	
+	totalSize_readable=$(echo "scale=2; $totalSize / 1024 / 1024" | bc)
+	
 	echo "total file : $totalFiles"
-	echo "total size : $(($totalSize/1000)).$(($totalSize%1000)) mb"
+	echo "Total size: $totalSize_readable MB"
 	echo "total lines on those files : $totalLine"
+	
+	read -p "Please hit enter to continue"
 fi
